c792031467cf949288dce6ebdd675244e72658e8
Benoit BERTRAND <benoit.bertrand@live.fr>
2021-10-13 23:57:08 +0200

    Tu croyais que ça fonctionnais, et bien ça marche plus...

--- src/main/java/org/github/boutanche/applicationcontext/AccountService.java
+++ src/main/java/org/github/boutanche/applicationcontext/AccountService.java
@@ -3,4 +3,7 @@ package org.github.boutanche.applicationcontext;
 public class AccountService {
     public AccountService(AccountRepository accountRepository) {
     }
+    public String transformToString(){
+        return "çaMarche";
+    }
 }
--- src/main/java/org/github/boutanche/hello.java
+++ src/main/java/org/github/boutanche/hello.java
@@ -1,6 +1,10 @@
 package org.github.boutanche;
 
+import org.github.boutanche.applicationcontext.AccountConfig;
+import org.github.boutanche.applicationcontext.AccountService;
 import org.github.boutanche.perso.MonSingleton;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class hello {
     public static void main(String[] arg){
@@ -11,6 +15,10 @@ public class hello {
 
         boolean test = s1==s2;
         System.out.println("s1==s2:"+test);
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(AccountConfig.class);
+        AccountService accountService = context.getBean(AccountService.class);
+        System.out.println(accountService.transformToString());
 
 
     }
