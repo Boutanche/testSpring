5d49a97912205449cbab1b13eda317fdf21fcbff
b.bertrand <benoit.bertand@neo-soft.fr>
2021-10-14 09:38:08 +0200

    add testSpring.java

--- src/main/java/org/github/boutanche/testSpring.java
+++ src/main/java/org/github/boutanche/testSpring.java
@@ -2,28 +2,14 @@ package org.github.boutanche;
 
 import org.github.boutanche.applicationcontext.AccountConfig;
 import org.github.boutanche.applicationcontext.AccountService;
-import org.github.boutanche.perso.MonSingleton;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-
-public class hello {
-
-    /*
-
-    public static void main(String[] arg){
+public class testSpring {
+    public static void main(String[] arg) {
         System.out.println("Hello");
-
-        MonSingleton s1 = MonSingleton.getInstance();
-        MonSingleton s2 = MonSingleton.getInstance();
-
-        boolean test = s1==s2;
-        System.out.println("s1==s2:"+test);
-
         ApplicationContext context = new AnnotationConfigApplicationContext(AccountConfig.class);
         AccountService accountService = context.getBean(AccountService.class);
         System.out.println(accountService.transformToString());
-
     }
-    */
 }
